e058b9097a88bbf353b2b9341d06d76d078b7ddc
Pántya Barnabás <pantya.barnabas@gmail.com>
2024-03-22 21:34:34 +0100

    Navigációs gombok hozzáadása a MainForm-hoz

--- Project/Saturn_Client/MainForm.Designer.cs
+++ Project/Saturn_Client/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Saturn_Client
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            userDataButton = new Button();
+            studiesButton = new Button();
+            subjectsButton = new Button();
+            examsButton = new Button();
+            financesButton = new Button();
+            SuspendLayout();
+            // 
+            // userDataButton
+            // 
+            userDataButton.Location = new Point(71, 83);
+            userDataButton.Name = "userDataButton";
+            userDataButton.Size = new Size(152, 56);
+            userDataButton.TabIndex = 0;
+            userDataButton.Text = "Saját adatok";
+            userDataButton.UseVisualStyleBackColor = true;
+            // 
+            // studiesButton
+            // 
+            studiesButton.Location = new Point(300, 83);
+            studiesButton.Name = "studiesButton";
+            studiesButton.Size = new Size(152, 56);
+            studiesButton.TabIndex = 1;
+            studiesButton.Text = "Tanulmányok";
+            studiesButton.UseVisualStyleBackColor = true;
+            // 
+            // subjectsButton
+            // 
+            subjectsButton.Location = new Point(529, 83);
+            subjectsButton.Name = "subjectsButton";
+            subjectsButton.Size = new Size(152, 56);
+            subjectsButton.TabIndex = 2;
+            subjectsButton.Text = "Tárgyak";
+            subjectsButton.UseVisualStyleBackColor = true;
+            // 
+            // examsButton
+            // 
+            examsButton.Location = new Point(752, 83);
+            examsButton.Name = "examsButton";
+            examsButton.Size = new Size(152, 56);
+            examsButton.TabIndex = 3;
+            examsButton.Text = "Vizsgázás";
+            examsButton.UseVisualStyleBackColor = true;
+            // 
+            // financesButton
+            // 
+            financesButton.Location = new Point(972, 83);
+            financesButton.Name = "financesButton";
+            financesButton.Size = new Size(152, 56);
+            financesButton.TabIndex = 4;
+            financesButton.Text = "Pénzügyek";
+            financesButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1262, 673);
+            Controls.Add(financesButton);
+            Controls.Add(examsButton);
+            Controls.Add(subjectsButton);
+            Controls.Add(studiesButton);
+            Controls.Add(userDataButton);
+            Name = "MainForm";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button userDataButton;
+        private Button studiesButton;
+        private Button subjectsButton;
+        private Button examsButton;
+        private Button financesButton;
     }
 }
