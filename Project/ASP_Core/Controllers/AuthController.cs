a067b53b22d5d511dd8d62d639463aeb215f3133
herbakmarcell <herbakmarcell@gmail.com>
2024-04-02 22:51:10 +0200

    AuthController alapjai

--- Project/ASP_Core/Controllers/AuthController.cs
+++ Project/ASP_Core/Controllers/AuthController.cs
@@ -1,7 +1,33 @@
-﻿namespace ASP_Core.Controllers
-{
-    public class AuthController
-    {
+﻿using ASP_Core.Database;
+using ASP_Core.Models.Auth;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
+namespace ASP_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly SaturnContext saturnContext;
+
+        public AuthController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            if ()
+            {
+
+            }
+        }
     }
 }
