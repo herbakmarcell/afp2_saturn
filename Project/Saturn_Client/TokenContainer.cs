817b9fc8c6347672b5146c9f052d1ab97b5c889c
herbakmarcell <herbakmarcell@gmail.com>
2024-04-22 22:35:48 +0200

    Logika hozzáadása admincheckhez

--- Project/Saturn_Client/TokenContainer.cs
+++ Project/Saturn_Client/TokenContainer.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Saturn_Client
 {
     public static class TokenContainer
     {
         public static string? Token {  get; set; }
-        
+        public static bool IsAdmin
+        {
+            get
+            {
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var jwtToken = tokenHandler.ReadJwtToken(Token);
+
+                var claims = jwtToken.Claims;
+
+                return claims.FirstOrDefault(c => c.Type == "roles").Value.Split(',').Contains("Admin");
+            }
+        }
     }
 }
