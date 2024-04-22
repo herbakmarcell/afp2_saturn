524de84a70ac605750d2c8f0c5ab5eaa439adb92
Pántya Barnabás <pantya.barnabas@gmail.com>
2024-04-23 01:10:40 +0200

    JWT nuget hozzáadása & labelek előkészítése

--- Project/Saturn_Client/TokenContainer.cs
+++ Project/Saturn_Client/TokenContainer.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Saturn_Client
 {
     public static class TokenContainer
     {
         public static string? Token {  get; set; }
-        
+        public static string? GetSaturnCode {
+            get
+            {
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var jwtToken = tokenHandler.ReadJwtToken(Token);
+
+                var claims = jwtToken.Claims;
+
+                return claims.FirstOrDefault(c => c.Type == "saturnCode").Value.ToString();
+            }
+        }
     }
 }
--- Project/Saturn_Client/UserDataForm.cs
+++ Project/Saturn_Client/UserDataForm.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Net;
+using System.Security.Claims;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,12 +17,49 @@ namespace Saturn_Client
 {
     public partial class UserDataForm : Form
     {
+        private RestClient client;
+
         public UserDataForm()
         {
             InitializeComponent();
+            client = new RestClient("https://localhost:7204/api/Auth");
         }
+
+
+        private async void loadUserData()
+        {
+            var request = new RestRequest("/user", Method.Get);
+            try
+            {
+                request.AddHeader("saturnCode", TokenContainer.GetSaturnCode);
+                var response = await client.ExecuteAsync(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var responseContent = response.Content;
+                    var responseData = JsonSerializer.Deserialize<Response<UserDataResponse>>(response.Content);
+                    saturnCodeLabel.Text = responseData.resource.saturnCode;
+                    familyNameLabel.Text = responseData.resource.lastName;
+                    givenNameLabel.Text = responseData.resource.firstName;
+                    emailLabel.Text = responseData.resource.email;
+                    phoneLabel.Text = responseData.resource.phoneNumber;
+                }
+                else if (response.StatusCode == HttpStatusCode.BadRequest)
+                {
+                    var responseContent = response.Content;
+                    Response<string> temp = JsonSerializer.Deserialize<Response<string>>(responseContent);
+                    MessageBox.Show("Hibás felhasználónév vagy jelszó! \nHibaüzenet: " + temp.message);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Váratlan hiba! \nBővebben: " + ex.Message);
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            loadUserData();
         }
 
         private void HelpButton_Click(object sender, EventArgs e)
