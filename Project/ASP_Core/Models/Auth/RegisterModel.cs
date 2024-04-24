507e0b9332e29cc5733a74e7d6ed611f14c444df
herbakmarcell <herbakmarcell@gmail.com>
2024-04-24 12:31:34 +0200

    Utó és vezetéknév cserélve

--- Project/ASP_Core/Models/Auth/RegisterModel.cs
+++ Project/ASP_Core/Models/Auth/RegisterModel.cs
@@ -9,11 +9,11 @@ namespace ASP_Core.Models.Auth
         [StringLength(16, MinimumLength = 3, ErrorMessage = "A jelszónak legalább 3 és legfeljebb 16 karakter hosszúnak kell lennie.")]
         public string? Password { get; set; }
 
-        [Required(ErrorMessage = "Utónév szükséges")]
+        [Required(ErrorMessage = "Vezetéknév szükséges")]
         [RegularExpression(@"^[A-Za-zÁÉÍÓÖŐÚÜŰáéíóöőúüű'-]+$", ErrorMessage = "Az utónév nem megfelelő formátumú.")]
         public string? LastName { get; set; }
 
-        [Required(ErrorMessage = "Vezetéknév szükséges")]
+        [Required(ErrorMessage = "Utónév szükséges")]
         [RegularExpression(@"^[A-Za-zÁÉÍÓÖŐÚÜŰáéíóöőúüű'-]+$", ErrorMessage = "A vezetéknév nem megfelelő formátumú.")]
         public string? FirstName { get; set; }
 
