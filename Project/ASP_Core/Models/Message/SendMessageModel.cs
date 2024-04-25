e06a6e1d1ec1ad4a6a422fe35ebebf9d4b9cc3a9
herbakmarcell <herbakmarcell@gmail.com>
2024-04-26 00:37:54 +0200

    Üzenet küldő modell és válaszmodell

---
+++ Project/ASP_Core/Models/Message/SendMessageModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Core.Models.Message
+{
+    public class SendMessageModel
+    {
+        public string? Sender { get; set; }
+        [Required(ErrorMessage = "Tárgy megadása szükséges")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Üzenet megadása")]
+        [StringLength(5000, MinimumLength = 1, ErrorMessage = "Az üzenetnek tartalmaznia kell legalább 1 karaktert.")]
+        public string Content { get; set; }
+        [Required(ErrorMessage = "Címzett(ek) megadása szükséges")]
+        public List<string> Receivers { get; set; }
+    }
+}
---
+++ Project/ASP_Core/Models/Responses/SendMessageResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Core.Models.Responses
+{
+    public class SendMessageResponse
+    {
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public List<string> Receivers { get; set; }
+    }
+}
