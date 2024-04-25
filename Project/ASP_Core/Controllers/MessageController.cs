b3f79874e979cb9fee3ee80abb436cbb896203c6
herbakmarcell <herbakmarcell@gmail.com>
2024-04-26 00:38:29 +0200

    api pont létrehozása

--- Project/ASP_Core/Controllers/MessageController.cs
+++ Project/ASP_Core/Controllers/MessageController.cs
@@ -1,5 +1,6 @@
 ﻿using ASP_Core.Database.Models;
 using ASP_Core.Models;
+using ASP_Core.Models.Message;
 using ASP_Core.Models.Responses;
 using ASP_Core.Services.Auth;
 using Microsoft.AspNetCore.Authorization;
@@ -34,6 +35,29 @@ namespace ASP_Core.Controllers
             List<ReceivedMessageResponse> receivedMessageResponses = messageService.GetReceivedMessages(saturnCode, sender);
             if (receivedMessageResponses == null) return BadRequest(new Response<string>("No existing user with messages."));
             return new OkObjectResult(new Response<List<ReceivedMessageResponse>>(receivedMessageResponses));
+        }
+
+        [HttpGet]
+        [Authorize()]
+        [Route("sent")]
+        public ActionResult<Response<List<SentMessageResponse>>> GetSentMessages(string? sender)
+        {
+            sender ??= commonService.TokenWithSaturn(User.Claims);
+            if (!commonService.TokenHasRole(User.Claims, "Admin")) return BadRequest(new Response<string>("Insufficent permission to watch others mailbox."));
+            List<SentMessageResponse> sentMessageResponses = messageService.GetSentMessages(sender);
+            if (sentMessageResponses == null) return BadRequest(new Response<string>("No existing user with messages."));
+            return new OkObjectResult(new Response<List<SentMessageResponse>>(sentMessageResponses));
+        }
+
+        [HttpPost]
+        [Authorize()]
+        [Route("send")]
+        public ActionResult<Response<SendMessageResponse>> SendMessage([FromBody] SendMessageModel messageModel)
+        {
+            messageModel.Sender = commonService.TokenWithSaturn(User.Claims);
+            SendMessageResponse messageResponse = messageService.SendMessage(messageModel);
+            if (messageResponse == null) return BadRequest(new Response<string>("Invalid sender or no real recievers."));
+            return new OkObjectResult(new Response<SendMessageResponse>(messageResponse));
         }
     }
 }
