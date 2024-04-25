5a1b5dd422f9d9f5180c921e1e86e9c0492a2248
herbakmarcell <herbakmarcell@gmail.com>
2024-04-25 22:35:17 +0200

    MessageService alapja

---
+++ Project/ASP_Core/Services/Auth/MessageService.cs
@@ -0,0 +1,40 @@
+﻿using ASP_Core.Database;
+using ASP_Core.Database.Models;
+using ASP_Core.Models.Auth;
+using ASP_Core.Models.Responses;
+using System.Security.Claims;
+
+namespace ASP_Core.Services.Auth
+{
+    public interface IMessageService
+    {
+        public List<ReceivedMessageResponse> GetReceivedMessages(string? saturnCode, string? sender);
+    }
+
+    public class MessageService : IMessageService
+    {
+        private readonly SaturnContext saturnContext;
+        public MessageService(SaturnContext saturnContext)
+        {
+            this.saturnContext = saturnContext;
+        }
+
+
+        public List<ReceivedMessageResponse> GetReceivedMessages(string saturnCode, string? sender)
+        {
+            List<MessageModel> contextReceivedMessages = saturnContext.GetReceivedMessage(saturnCode, sender);
+            if (contextReceivedMessages == null) return null;
+
+            List<ReceivedMessageResponse> receivedMessages = contextReceivedMessages.Select(message => new ReceivedMessageResponse
+            {
+                Id = message.Id,
+                Subject = message.Subject,
+                Content = message.Content,
+                Sender = message.Sender.SaturnCode,
+                Receivers = message.Receivers.Select(user => user.SaturnCode).ToList()
+            }).ToList();
+
+            return receivedMessages;
+        }
+    }
+}
