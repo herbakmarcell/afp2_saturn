bb51ca5fc4089170ca06a8f98a5eabc9e8cb6a3c
herbakmarcell <herbakmarcell@gmail.com>
2024-04-25 22:34:48 +0200

    messagecontroller alapja

---
+++ Project/ASP_Core/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using ASP_Core.Database.Models;
+using ASP_Core.Models;
+using ASP_Core.Models.Responses;
+using ASP_Core.Services.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ASP_Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly AuthIService authService;
+        private readonly ICommonService commonService;
+        private readonly IMessageService messageService;
+
+        public MessageController(AuthIService authService, ICommonService commonService, IMessageService messageService)
+        {
+            this.authService = authService;
+            this.commonService = commonService;
+            this.messageService = messageService;
+        }
+
+        [HttpGet]
+        [Authorize()]
+        [Route("received")]
+        public ActionResult<Response<List<ReceivedMessageResponse>>> GetReceivedMessages([FromHeader] string? saturnCode, [FromHeader] string? sender)
+        {
+            saturnCode ??= commonService.TokenWithSaturn(User.Claims);
+            sender ??= null;
+
+            List<ReceivedMessageResponse> receivedMessageResponses = messageService.GetReceivedMessages(saturnCode, sender);
+            if (receivedMessageResponses == null) return BadRequest(new Response<string>("No existing user with messages."));
+            return null;
+        }
+    }
+}
