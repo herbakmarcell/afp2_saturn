d07a824dc5a06c4867673221b5014f1c04ca1846
herbakmarcell <herbakmarcell@gmail.com>
2024-04-25 20:11:16 +0200

    MessageModel létrehozva

---
+++ Project/ASP_Core/Database/Models/MessageModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP_Core.Database.Models
+{
+    public class MessageModel
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public User Sender { get; set; }
+        public List<User> Receivers { get; set; }
+
+    }
+}
