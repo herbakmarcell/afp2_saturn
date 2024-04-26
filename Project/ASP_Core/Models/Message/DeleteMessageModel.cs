01c374779b803e5bd2c61e5089a7d451cb76a1df
herbakmarcell <herbakmarcell@gmail.com>
2024-04-26 16:06:16 +0200

    üzenet törlése modellek

---
+++ Project/ASP_Core/Models/Message/DeleteMessageModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Core.Models.Message
+{
+    public class DeleteMessageModel
+    {
+        public string? SaturnCode { get; set; }
+        [Required(ErrorMessage = "Az üzenet tárgyának megadása szükséges.")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Szükséges tudni, hogy kimenő vagy bejövő az üzenet.")]
+        public bool IsSent { get; set; }
+    }
+}
---
+++ Project/ASP_Core/Models/Responses/DeleteMessageResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP_Core.Models.Responses
+{
+    public class DeleteMessageResponse
+    {
+        public string? SaturnCode { get; set; }
+        public string? Subject { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
