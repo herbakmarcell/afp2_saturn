b5304d131ebf2121de4148980f9cf0c7eab594d2
FourthRuffle <fodor.gyozo.benedek@gmail.com>
2024-05-07 19:47:51 +0200

    vizsgakezelés frontend
    examform - label eltávolítás, datagridview + gomb

--- Project/Saturn_Client/ExamsForm.Designer.cs
+++ Project/Saturn_Client/ExamsForm.Designer.cs
@@ -29,113 +29,20 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
-            label8 = new Label();
             HelpButton = new Button();
             toolTip1 = new ToolTip(components);
+            dataGridView1 = new DataGridView();
+            subject = new DataGridViewTextBoxColumn();
+            subjectCode = new DataGridViewTextBoxColumn();
+            course = new DataGridViewTextBoxColumn();
+            type = new DataGridViewTextBoxColumn();
+            startingTime = new DataGridViewTextBoxColumn();
+            personLimit = new DataGridViewTextBoxColumn();
+            classroom = new DataGridViewTextBoxColumn();
+            teacher = new DataGridViewTextBoxColumn();
+            myExamsButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Comic Sans MS", 9F);
-            label1.ForeColor = Color.Maroon;
-            label1.Location = new Point(38, 21);
-            label1.Name = "label1";
-            label1.Size = new Size(50, 20);
-            label1.TabIndex = 0;
-            label1.Text = "Tárgy";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.Transparent;
-            label2.Font = new Font("Comic Sans MS", 9F);
-            label2.ForeColor = Color.Maroon;
-            label2.Location = new Point(88, 21);
-            label2.Name = "label2";
-            label2.Size = new Size(76, 20);
-            label2.TabIndex = 1;
-            label2.Text = "Tárgykód";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.BackColor = Color.Transparent;
-            label3.Font = new Font("Comic Sans MS", 9F);
-            label3.ForeColor = Color.Maroon;
-            label3.Location = new Point(163, 21);
-            label3.Name = "label3";
-            label3.Size = new Size(56, 20);
-            label3.TabIndex = 2;
-            label3.Text = "Kurzus";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.BackColor = Color.Transparent;
-            label4.Font = new Font("Comic Sans MS", 9F);
-            label4.ForeColor = Color.Maroon;
-            label4.Location = new Point(221, 21);
-            label4.Name = "label4";
-            label4.Size = new Size(46, 20);
-            label4.TabIndex = 3;
-            label4.Text = "Típus";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.BackColor = Color.Transparent;
-            label5.Font = new Font("Comic Sans MS", 9F);
-            label5.ForeColor = Color.Maroon;
-            label5.Location = new Point(271, 21);
-            label5.Name = "label5";
-            label5.Size = new Size(58, 20);
-            label5.TabIndex = 4;
-            label5.Text = "Kezdés";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.BackColor = Color.Transparent;
-            label6.Font = new Font("Comic Sans MS", 9F);
-            label6.ForeColor = Color.Maroon;
-            label6.Location = new Point(333, 21);
-            label6.Name = "label6";
-            label6.Size = new Size(68, 20);
-            label6.TabIndex = 5;
-            label6.Text = "Fő/Limit";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.BackColor = Color.Transparent;
-            label7.Font = new Font("Comic Sans MS", 9F);
-            label7.ForeColor = Color.Maroon;
-            label7.Location = new Point(403, 21);
-            label7.Name = "label7";
-            label7.Size = new Size(53, 20);
-            label7.TabIndex = 6;
-            label7.Text = "Terem";
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.BackColor = Color.Transparent;
-            label8.Font = new Font("Comic Sans MS", 9F);
-            label8.ForeColor = Color.Maroon;
-            label8.Location = new Point(459, 21);
-            label8.Name = "label8";
-            label8.Size = new Size(60, 20);
-            label8.TabIndex = 7;
-            label8.Text = "Oktató";
             // 
             // HelpButton
             // 
@@ -145,50 +52,107 @@
             HelpButton.FlatAppearance.MouseOverBackColor = Color.Transparent;
             HelpButton.FlatStyle = FlatStyle.Flat;
             HelpButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            HelpButton.Location = new Point(999, 16);
-            HelpButton.Margin = new Padding(3, 4, 3, 4);
+            HelpButton.Location = new Point(874, 12);
             HelpButton.Name = "HelpButton";
-            HelpButton.Size = new Size(31, 36);
+            HelpButton.Size = new Size(27, 27);
             HelpButton.TabIndex = 12;
             HelpButton.Text = "?";
             toolTip1.SetToolTip(HelpButton, "Használati útmutató");
             HelpButton.UseVisualStyleBackColor = false;
             HelpButton.Click += HelpButton_Click;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { subject, subjectCode, course, type, startingTime, personLimit, classroom, teacher });
+            dataGridView1.Location = new Point(37, 65);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(843, 150);
+            dataGridView1.TabIndex = 13;
+            // 
+            // subject
+            // 
+            subject.HeaderText = "Tárgy";
+            subject.Name = "subject";
+            // 
+            // subjectCode
+            // 
+            subjectCode.HeaderText = "Tárgykód";
+            subjectCode.Name = "subjectCode";
+            // 
+            // course
+            // 
+            course.HeaderText = "Kurzus";
+            course.Name = "course";
+            // 
+            // type
+            // 
+            type.HeaderText = "Típus";
+            type.Name = "type";
+            // 
+            // startingTime
+            // 
+            startingTime.HeaderText = "Kezdés";
+            startingTime.Name = "startingTime";
+            // 
+            // personLimit
+            // 
+            personLimit.HeaderText = "Fő/Limit";
+            personLimit.Name = "personLimit";
+            // 
+            // classroom
+            // 
+            classroom.HeaderText = "Terem";
+            classroom.Name = "classroom";
+            // 
+            // teacher
+            // 
+            teacher.HeaderText = "Oktató";
+            teacher.Name = "teacher";
+            // 
+            // myExamsButton
+            // 
+            myExamsButton.BackColor = Color.Transparent;
+            myExamsButton.FlatAppearance.BorderSize = 0;
+            myExamsButton.FlatStyle = FlatStyle.Flat;
+            myExamsButton.Font = new Font("Comic Sans MS", 9F, FontStyle.Underline, GraphicsUnit.Point, 238);
+            myExamsButton.Location = new Point(753, 30);
+            myExamsButton.Name = "myExamsButton";
+            myExamsButton.Size = new Size(115, 34);
+            myExamsButton.TabIndex = 14;
+            myExamsButton.Text = "Felvett vizsgáim";
+            myExamsButton.UseVisualStyleBackColor = false;
+            // 
             // ExamsForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.dataBackFinal;
-            ClientSize = new Size(1091, 462);
+            ClientSize = new Size(955, 346);
+            Controls.Add(myExamsButton);
+            Controls.Add(dataGridView1);
             Controls.Add(HelpButton);
-            Controls.Add(label8);
-            Controls.Add(label7);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
             ForeColor = Color.DarkOrange;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "ExamsForm";
             Text = "ExamsForm";
             toolTip1.SetToolTip(this, "Használati útmutató");
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private Label label6;
-        private Label label7;
-        private Label label8;
         private Button HelpButton;
         private ToolTip toolTip1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn subject;
+        private DataGridViewTextBoxColumn subjectCode;
+        private DataGridViewTextBoxColumn course;
+        private DataGridViewTextBoxColumn type;
+        private DataGridViewTextBoxColumn startingTime;
+        private DataGridViewTextBoxColumn personLimit;
+        private DataGridViewTextBoxColumn classroom;
+        private DataGridViewTextBoxColumn teacher;
+        private Button myExamsButton;
     }
 }
