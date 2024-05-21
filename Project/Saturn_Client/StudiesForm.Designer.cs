0869657f86c807f75071869425938892e6bfb3b0
FourthRuffle <fodor.gyozo.benedek@gmail.com>
2024-05-21 20:23:04 +0200

    frontend tanulmányok

--- Project/Saturn_Client/StudiesForm.Designer.cs
+++ Project/Saturn_Client/StudiesForm.Designer.cs
@@ -34,41 +34,49 @@
             label3 = new Label();
             HelpButton = new Button();
             toolTip1 = new ToolTip(components);
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Comic Sans MS", 7.8F);
-            label1.ForeColor = Color.Maroon;
-            label1.Location = new Point(73, 37);
+            label1.Font = new Font("Comic Sans MS", 14.25F);
+            label1.ForeColor = Color.DarkOrange;
+            label1.Location = new Point(55, 55);
             label1.Name = "label1";
-            label1.Size = new Size(108, 15);
+            label1.Size = new Size(297, 26);
             label1.TabIndex = 1;
-            label1.Text = "Jogviszony kezdete";
+            label1.Text = "Jogviszony kezdete: 2024.09.01";
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
-            label2.Font = new Font("Comic Sans MS", 7.8F);
-            label2.ForeColor = Color.Maroon;
-            label2.Location = new Point(73, 85);
+            label2.Font = new Font("Comic Sans MS", 14.25F);
+            label2.ForeColor = Color.DarkOrange;
+            label2.Location = new Point(55, 103);
             label2.Name = "label2";
-            label2.Size = new Size(86, 15);
+            label2.Size = new Size(264, 26);
             label2.TabIndex = 2;
-            label2.Text = "Felvétel dátuma";
+            label2.Text = "Felvétel dátuma: 2024.07.21";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
-            label3.Font = new Font("Comic Sans MS", 7.8F);
-            label3.ForeColor = Color.Maroon;
-            label3.Location = new Point(73, 132);
+            label3.Font = new Font("Comic Sans MS", 14.25F);
+            label3.ForeColor = Color.DarkOrange;
+            label3.Location = new Point(55, 150);
             label3.Name = "label3";
-            label3.Size = new Size(80, 15);
+            label3.Size = new Size(137, 26);
             label3.TabIndex = 3;
             label3.Text = "Státusz: Aktív";
             // 
@@ -81,7 +89,7 @@
             HelpButton.FlatStyle = FlatStyle.Flat;
             HelpButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
             HelpButton.ForeColor = Color.DarkOrange;
-            HelpButton.Location = new Point(874, 12);
+            HelpButton.Location = new Point(985, 30);
             HelpButton.Name = "HelpButton";
             HelpButton.Size = new Size(27, 27);
             HelpButton.TabIndex = 12;
@@ -90,12 +98,116 @@
             HelpButton.UseVisualStyleBackColor = false;
             HelpButton.Click += HelpButton_Click;
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Comic Sans MS", 14.25F);
+            label4.ForeColor = Color.DarkOrange;
+            label4.Location = new Point(55, 192);
+            label4.Name = "label4";
+            label4.Size = new Size(328, 26);
+            label4.TabIndex = 13;
+            label4.Text = "Pénzügyi státusz: Állami ösztöndíjas";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Comic Sans MS", 14.25F);
+            label5.ForeColor = Color.DarkOrange;
+            label5.Location = new Point(55, 236);
+            label5.Name = "label5";
+            label5.Size = new Size(427, 26);
+            label5.TabIndex = 14;
+            label5.Text = "Képzés neve: Programtervező informatikus BSc";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Comic Sans MS", 14.25F);
+            label6.ForeColor = Color.DarkOrange;
+            label6.Location = new Point(55, 278);
+            label6.Name = "label6";
+            label6.Size = new Size(160, 26);
+            label6.TabIndex = 15;
+            label6.Text = "Tagozat: Nappali";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Comic Sans MS", 14.25F);
+            label7.ForeColor = Color.DarkOrange;
+            label7.Location = new Point(514, 103);
+            label7.Name = "label7";
+            label7.Size = new Size(525, 26);
+            label7.TabIndex = 16;
+            label7.Text = "Felvett választható modul: Szoftverfejlesztő informatikus";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Comic Sans MS", 14.25F);
+            label8.ForeColor = Color.DarkOrange;
+            label8.Location = new Point(514, 192);
+            label8.Name = "label8";
+            label8.Size = new Size(163, 26);
+            label8.TabIndex = 17;
+            label8.Text = "Félévek száma: 6";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Comic Sans MS", 14.25F);
+            label9.ForeColor = Color.DarkOrange;
+            label9.Location = new Point(514, 150);
+            label9.Name = "label9";
+            label9.Size = new Size(137, 26);
+            label9.TabIndex = 18;
+            label9.Text = "Nyelv: magyar";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Comic Sans MS", 14.25F);
+            label10.ForeColor = Color.DarkOrange;
+            label10.Location = new Point(55, 323);
+            label10.Name = "label10";
+            label10.Size = new Size(236, 26);
+            label10.TabIndex = 19;
+            label10.Text = "Program típus:Alapképzés";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.Font = new Font("Comic Sans MS", 14.25F);
+            label11.ForeColor = Color.DarkOrange;
+            label11.Location = new Point(514, 55);
+            label11.Name = "label11";
+            label11.Size = new Size(231, 26);
+            label11.TabIndex = 20;
+            label11.Text = "Képzés kódja: PTI-SZ-N";
+            // 
             // StudiesForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.dataBackFinal;
             ClientSize = new Size(1111, 494);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
             Controls.Add(HelpButton);
             Controls.Add(label3);
             Controls.Add(label2);
@@ -114,5 +226,13 @@
         private Label label3;
         private Button HelpButton;
         private ToolTip toolTip1;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
     }
 }
